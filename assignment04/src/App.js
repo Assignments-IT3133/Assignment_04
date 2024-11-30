b8910d33c32a9d8e02accab9e3a0d73be91e4bfc
PasinduNuwan <154910776+Pasindunuwan2000@users.noreply.github.com>
2024-11-30 11:25:28 +0530

    Update App.js

--- assignment04/src/App.js
+++ assignment04/src/App.js
@@ -1,11 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
-import figure from './components/Figure';
+import { useState } from 'react';
 import Figure from './components/Figure';
 
 function App() {
+  const[currentFontSize,setFontSize]=useState('14px')
+
+  const changeFontSize=(size)=>{
+    switch(size){
+      case 'small':
+        setFontSize('18px');
+        break;
+      case 'mediam':
+        setFontSize('22px');
+        break;
+      case 'large':
+        setFontSize('26px');
+        break;
+      default:
+        setFontSize('20px')
+    }
+  }
   return (
-    <div className="App">
+    <div className="App" style={{fontSize:currentFontSize}}>
+      <p>Welcome</p>
+      <button onClick={()=>changeFontSize('small')}>Small</button>
+      <button onClick={()=>changeFontSize('mediam')}>Mediam</button>
+      <button onClick={()=>changeFontSize('large')}>Large</button>
       <Figure/>
     </div>
   );
